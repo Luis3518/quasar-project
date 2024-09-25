3595525147395c93a31965cf275e48495b669d33
Luis Miguel Rodriguez <48697966+Luis3518@users.noreply.github.com>
2024-09-25 14:10:48 -0300

    add-files-init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>{{ message }}</h1>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  data() {
+    return {
+      message: 'Hola Mundo!'
+    }
+  }
+})
+</script>
